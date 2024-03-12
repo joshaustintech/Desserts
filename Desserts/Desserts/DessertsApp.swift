81c102a75850ff66c0f51f36f1ac85a7cc946805
Josh Austin <83935880+joshaustintech@users.noreply.github.com>
2024-03-12 12:09:40 -0500

    initial project creation in XCode

---
+++ Desserts/Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Joshua Austin on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
